5d2a885346483dd255df094e440d97af4e9fa86e
Carsten <df3xc@web.de>
2019-03-03 02:33:04 +0100

    Update libpigpio.c

--- libpigpio/libpigpio.c
+++ libpigpio/libpigpio.c
@@ -13,7 +13,13 @@
 // sudo cp libpigpio.so /lib
 
 // Access from ARM Running Linux
-#define BCM2708_PERI_BASE 0x20000000
+
+// Raspberry 1
+//#define BCM2708_PERI_BASE 0x20000000
+
+// Raspberry 2 and 3
+#define BCM2708_PERI_BASE 0x3F000000
+
 #define GPIO_BASE (BCM2708_PERI_BASE + 0x200000) /* GPIO controller */
 
 #include <stdio.h>
